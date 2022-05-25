a5118a5a004702f3ed88f5853ac0a5ffef31efc7
Himanshu-Chauhan1 <102967623+Himanshu-Chauhan1@users.noreply.github.com>
2022-05-25 13:07:23 +0530

    apis added

--- src/routes/route.js
+++ src/routes/route.js
@@ -6,8 +6,8 @@ const userController = require("../controllers/userController")
 // User routes
 router.post('/register', userController.createUser);
 router.post('/login', userController.loginUser);
-router.get('/user/:userId/profile',userController.getUserProfile)
-router.put('/user/:userId/profile',userController.updateUserProfile)
+router.get('/user/:userId/profile', userController.getUserProfile)
+router.put('/user/:userId/profile', userController.updateUserProfile)
 
 
 
